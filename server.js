80407f8478e2768b00a3e8dab681edef42e9eaad
yutashimizu0612 <s08388yskh@gmail.com>
2020-09-14 21:11:06 +0900

    post送信下準備

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
 
+const users = [];
+
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', function (req, res) {
   res.render('pages/index', { name: 'test' });
@@ -11,7 +14,15 @@ app.get('/register', function (req, res) {
   res.render('pages/register');
 });
 
+app.post('/register', function (req, res) {
+  res.render('pages/register');
+});
+
 app.get('/login', function (req, res) {
+  res.render('pages/login');
+});
+
+app.post('/login', function (req, res) {
   res.render('pages/login');
 });
 
