0d86165ac651a952155f0b2a63a7dc0d42b7b3cd
yutashimizu0612 <s08388yskh@gmail.com>
2020-09-18 08:02:44 +0900

    ユーザ登録処理 / リダイレクト処理

--- server.js
+++ server.js
@@ -21,30 +21,52 @@ app.use(
   })
 );
 
-app.get('/', function (req, res) {
-  res.render('pages/index', { name: 'test' });
+// validation
+// Sessionでログイン状態を作る
+
+const redirectToLogin = (req, res, next) => {
+  if (!req.session.user) {
+    res.redirect('/login');
+  } else {
+    next();
+  }
+};
+
+const redirectToHome = (req, res, next) => {
+  if (req.session.user) {
+    res.redirect('/');
+  } else {
+    next();
+  }
+};
+
+app.get('/', redirectToLogin, function (req, res) {
+  console.log('トップ');
+  res.render('pages/index', { name: req.session.user.name });
 });
 
-app.get('/register', function (req, res) {
+app.get('/register', redirectToHome, function (req, res) {
   res.render('pages/register');
 });
 
 app.post('/register', function (req, res) {
-  const hashedPassword = bcrypt.hashSync(req.body.password, 10);
-  users.push({
-    id: Date.now().toString(),
-    name: req.body.name,
-    email: req.body.email,
-    password: hashedPassword,
-  });
+  const { name, email, password, confirm_password } = req.body;
+  if (name && email && password && confirm_password) {
+    const hashedPassword = bcrypt.hashSync(password, 10);
+    const newUser = {
+      id: Date.now().toString(),
+      name,
+      email,
+      password: hashedPassword,
+    };
+    users.push(newUser);
+    req.session.user = newUser;
+    console.log(req.session.user);
+  }
   res.redirect('/');
 });
 
-app.get('/login', function (req, res) {
-  res.render('pages/login');
-});
-
-app.post('/login', function (req, res) {
+app.get('/login', redirectToHome, function (req, res) {
   res.render('pages/login');
 });
 
